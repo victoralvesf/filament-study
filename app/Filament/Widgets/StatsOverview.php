edf38de8d64c031379bdf261bafa7a8059c35e20
Victor Alves <viictor.alvess12@gmail.com>
2025-06-08 11:55:34 -0300

    feat: add custom widgets to dashboard

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Course;
+use App\Models\Student;
+use App\Models\Teacher;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
+class StatsOverview extends BaseWidget
+{
+    protected static ?string $pollingInterval = null;
+
+    protected function getCards(): array
+    {
+        $active_students = Student::count();
+        $active_courses = Course::count();
+        $active_teachers = Teacher::count();
+
+        return [
+            Card::make('Alunos ativos', $active_students)
+                ->description('Quantidade de alunos presentes em um curso.')
+                ->descriptionIcon('heroicon-o-academic-cap')
+                ->color('primary'),
+            Card::make('Cursos ativos', $active_courses)
+                ->description('Quantidade de cursos ministrados no momento.')
+                ->descriptionIcon('heroicon-o-library')
+                ->color('primary'),
+            Card::make('Professores ativos', $active_teachers)
+                ->description('Quantidade de professores ministrando cursos no momento.')
+                ->descriptionIcon('heroicon-o-briefcase')
+                ->color('primary'),
+        ];
+    }
+}
